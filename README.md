003dad97ae49005ada9b34414ec7bde156b003db
Chris Cirelli <ccirelli2@gmail.com>
2023-04-30 11:03:21 -0400

    misc

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # everything-generative-ai-sandbox
 
-all work on libraries to learn generative ai.
+All work on libraries to learn generative ai.
+
+References
+==========================
+https://github.com/openai/chatgpt-retrieval-plugin#quickstart
+https://github.com/openai/chatgpt-retrieval-plugin
+https://platform.openai.com/docs/plugins/introduction
+https://openai.com/blog/chatgpt-plugins
+https://gptforwork.com/guides/openai-gpt3-temperature
+https://openai.com/pricing
+https://python.langchain.com/en/latest/modules/models/llms/examples/llm_caching.html
+https://docs.langchain.com/docs/components/models/language-model
--- chat-gpt-sandbox/scratch/scratch_chatgtp_retrieval_plugin.py
+++ chat-gpt-sandbox/scratch/scratch_chatgtp_retrieval_plugin.py
@@ -1,5 +1,25 @@
 """
+Retrieval Plugin
+======================
+- This is a plugin for ChatGPT that enables semantic search and retrieval of personal
+or organizational documents.  it allows users to obtain the most relevant document
+snippets from their data sources, such as files, notes, or emails by asking questions
+or expressing needs in natural language.
+
+Notes
+======================
+- It may be the case that we need first research the dependencies associated with this project.
+- It is likely the case that we need to know how to use vector databases and possibly
+have a subscription.
+- Docs:
+    - includes documentation for setting up and using each vector database provider,
+    webhook, and removing unused dependencies.
+
 References
 ======================
 1. https://github.com/openai/chatgpt-retrieval-plugin#quickstart
+2. ChatGPT Plugin Docs: https://openai.com/blog/chatgpt-plugins
+
+
+
 """
